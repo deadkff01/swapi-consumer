76375faf75c79b149928e33eda38092f0fbb2c98
deadkff01 <kafferdennis@gmail.com>
2019-11-16 22:27:40 -0300

    :fire: removing unused tests, Main component test refactoring and updating dependencies

--- src/components/App/App.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import App from './App'
-import { render } from '@testing-library/react'
-
-it('renders without crashing', () => {
-  const container = render(<App />)
-  expect(container).toMatchSnapshot()
-})
